a8b04c0b1bbb359f13b5857dde81f8283409457c
Szk1997 <357610140@qq.com>
2019-10-17 00:37:11 +1100

    finished quiz_5

--- COMP9021/COMP9021_quiz_5/quiz_5.py
+++ COMP9021/COMP9021_quiz_5/quiz_5.py
@@ -17,21 +17,66 @@
 import sys
 
 
+# 编码部分
 def encode(list_of_integers):
-    pass
-    # REPLACE pass ABOVE WITH YOUR CODE
+    return_code = ''
+    bin_list = []  # 对应的二进制转换列表
+    for e in list_of_integers:
+        bin_list.append(bin(e)[2:])
+    # print('the corresponding bin_list is : ', bin_list)
+    if len(bin_list) > 1:  # 列表中有大于一个元素
+        for bin_number in bin_list:
+            for letter in bin_number:
+                return_code = return_code + (letter * 2)
+            if bin_number == bin_list[-1]:  # 编码到最后一个元素则结束
+                break
+            return_code = return_code + '0'  # 每个元素之间编码时应该连接‘0’(按照规则)
+        # print('after encode, the corresponding bin number is :', return_code)
+        return int(return_code, 2)
+    else:  # 列表中只有一个元素
+        for letter in bin_list[0]:
+            return_code = return_code + (letter * 2)
+        print('after encode, the corresponding bin number is :', return_code)
+        return int(return_code, 2)
 
 
+# 解码部分
 def decode(integer):
-    pass
-    # REPLACE pass ABOVE WITH YOUR CODE
+    bin_value = bin(integer)[2:]
+    if len(bin_value) == 1:  # 特殊case：1
+        return None
+    else:  # 输入的integer > 1时的情况
+        result = ''  # 储存解码后的二进制数字
+        i = 0
+        while i < len(bin_value):
+            try:
+                if bin_value[i] == bin_value[i + 1]:  # 遇到‘00’或者‘11’时
+                    result = result + bin_value[i]
+                    # print(result)
+                    i += 2
+                else:
+                    if bin_value[i] == '1':  # 遇到‘10’时(会浪费编码位数)
+                        return None
+                    else:  # 遇到‘01’时
+                        i += 1
+                        result = result + ','  # 遇到‘01’，跳过0，并记录下一个数字
+                        # print(result)
+            except IndexError:
+                return None
+
+        # print('after decoding, the result is :', result)
+        temp_list = result.split(',')
+        return_list = []
+        for string in temp_list:
+            return_list.append(int(string, 2))
+        return return_list
 
 
 # We assume that user input is valid. No need to check
 # for validity, nor to take action in case it is invalid.
 print('Input either a strictly positive integer')
 the_input = eval(input('or a nonempty list of strictly positive integers: '))
-if type(the_input) is int:
+if type(the_input) is int:  # 输入是整数-->判断并进行解码
     print('  In base 2,', the_input, 'reads as', bin(the_input)[2:])
     decoding = decode(the_input)
     if decoding is None:
